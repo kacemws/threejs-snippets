41692fa3c197a7f757dc7201e5654f4b79b3bad5
kacemws <berras.belkacem@gmail.com>
2023-07-19 23:08:07 +0200

    using translations in the home page

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,18 @@
+import { useTranslations } from 'next-intl';
+import Text from '@/app/components/text/Text';
+import Title from '@/app/components/text/Title';
+import styles from '@/assets/styles/modules/_home.module.scss';
+
+export default function Home() {
+  const t = useTranslations('home');
+
+  return (
+    <main className={styles['home-page-container']}>
+      <div className={styles['home-page-background']} />
+      <div className={styles['home-page-content']}>
+        <Title>{t('title')}</Title>
+        <Text>{t('description')}</Text>
+      </div>
+    </main>
+  );
+}
